69dad78b06850109f778c0d9a024611ea51d62c1
JPereirax <jailsondev70@gmail.com>
2019-03-26 22:05:37 -0300

    adjust: :wrench: add annotation in repository.

--- src/main/java/com/zenfreela/zenid/repository/FollowRepository.java
+++ src/main/java/com/zenfreela/zenid/repository/FollowRepository.java
@@ -2,9 +2,11 @@ package com.zenfreela.zenid.repository;
 
 import com.zenfreela.zenid.model.Follow;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Repository
 public interface FollowRepository extends ReactiveMongoRepository<Follow, String> {
 
     Flux<Follow> findByEmail(String email);
